dc68b8982ab7efa33a7f7b03cc9a495e8e9a32c6
jota-esequiel <joaovitoresequielvieira@outlook.com>
2024-12-18 20:20:31 -0300

    Arquivos de configuração

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UniFood</title>
+
+    <?php 
+        require '../UniFood/vendor/autoload.php';
+        require '../UniFood/App/Config/config.php';
+
+        use App\Core\AssetManager;
+
+        AssetManager::addCss("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css");
+        AssetManager::addCss("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css");
+        AssetManager::renderCss(); 
+
+        AssetManager::addCss('../templates/CSS/setFrm1.css');
+        AssetManager::addCss('../templates/CSS/styleTablePad.css');
+        AssetManager::addCss('../templates/CSS/showToast.css');
+        AssetManager::addCss('../templates/CSS/subMenu.css');
+        AssetManager::addCss('../fontawesome/css/all.css');
+        AssetManager::renderCss();
+    ?>
+</head>
+<body>
+
+<?php
+    new \App\Core\RouterCore();
+?>
+
+<?php
+    AssetManager::addJs("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js");
+    AssetManager::addJs('../templates/JS/showToast.js');
+    AssetManager::renderJs();
+?>
+
+</body>
+</html>
