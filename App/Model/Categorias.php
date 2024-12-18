4bce49f89bca5e2a0abda63f85b5aeb204f55a7c
jota-esequiel <joaovitoresequielvieira@outlook.com>
2024-12-18 20:23:59 -0300

    Models

---
+++ App/Model/Categorias.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace App\Model;
+use App\Controller\Component\DataComponent;
+
+class Categorias {
+    public $Data;
+
+    public function __construct()
+    {
+        $this->Data = new DataComponent();
+        $this->Data->bdConnect('UNIFOOD');
+    }
+
+    public function sqlVisualizar() {
+        $strQuery = " SELECT * FROM categorias ";
+        return $strQuery;
+    }
+}
---
+++ App/Model/Usuarios.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App\Model;
+use App\Controller\Component\DataComponent;
+
+class Usuarios {
+    public $Data;
+
+    public function __construct()
+    {
+        $this->Data = new DataComponent();
+        $this->Data->bdConnect('UNIFOOD');
+    }
+
+    public function sqlUsuarios() {
+        $strQuery = " SELECT
+                         idUser,
+                         nomeuser,
+                         cpf,
+                         CASE sexo 
+                                WHEN 'M' THEN 'Masculino'
+                                WHEN 'F' THEN 'Feminino'
+                        END sexo
+                        FROM usuarios ";
+        return $strQuery;
+    }
+}
